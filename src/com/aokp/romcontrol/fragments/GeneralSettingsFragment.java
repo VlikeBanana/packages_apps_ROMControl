41706ade60456026310c498644a67b40b6b9060a
VlikeBanana <AndrolinRoot@googlemail.com>
2014-02-22 17:36:13 -0500

    Add Screenoff Animation

--- src/com/aokp/romcontrol/fragments/GeneralSettingsFragment.java
+++ src/com/aokp/romcontrol/fragments/GeneralSettingsFragment.java
@@ -22,6 +22,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.aokp.romcontrol.R;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+
+import static android.provider.Settings.System.SCREEN_OFF_ANIMATION;
+
+import java.util.List;
 
 public class GeneralSettingsFragment extends Fragment {
 
@@ -35,4 +62,56 @@ public class GeneralSettingsFragment extends Fragment {
 
         return v;
     }
+
+    private static ContentResolver mContentResolver;
+
+    private static final String KEY_SCREEN_OFF_ANIMATION = "screen_off_animation";
+
+    ListPreference mScreenOffAnimationPreference;
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mScreenOffAnimationPreference = (ListPreference) findPreference(KEY_SCREEN_OFF_ANIMATION);
+        final int currentAnimation = Settings.System.getInt(resolver, SCREEN_OFF_ANIMATION,
+                1 /* CRT-off */);
+        mScreenOffAnimationPreference.setValue(String.valueOf(currentAnimation));
+        mScreenOffAnimationPreference.setOnPreferenceChangeListener(this);
+        updateScreenOffAnimationPreferenceDescription(currentAnimation);
+
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mScreenOffAnimationPreference) {
+            int value = Integer.parseInt((String) objValue);
+            try {
+                Settings.System.putInt(getContentResolver(), SCREEN_OFF_ANIMATION, value);
+                updateScreenOffAnimationPreferenceDescription(value);
+            } catch (NumberFormatException e) {
+                Log.e(TAG, "could not persist screen-off animation setting", e);
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    private void updateScreenOffAnimationPreferenceDescription(int currentAnim) {
+        ListPreference preference = mScreenOffAnimationPreference;
+        String summary;
+        if (currentAnim < 0) {
+            // Unsupported value
+            summary = "";
+        } else {
+            final CharSequence[] entries = preference.getEntries();
+            final CharSequence[] values = preference.getEntryValues();
+            if (entries == null || entries.length == 0) {
+                summary = "";
+            } else {
+                summary = entries[currentAnim].toString();
+            }
+        }
+        preference.setSummary(summary);
+    }
+
 }
+
